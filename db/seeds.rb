139f4b281f2911ff990e7b856948c708833008c7
Jonathan Hoyt <hoyt@github.com>
2013-12-21 15:23:16 -0800

    rails_apps_composer: set up database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+puts 'ROLES'
+YAML.load(ENV['ROLES']).each do |role|
+  Role.find_or_create_by_name(role)
+  puts 'role: ' << role
+end
