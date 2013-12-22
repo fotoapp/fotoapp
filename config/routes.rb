03c0b66c5810c211190887867221f25c64630e91
Jonathan Hoyt <hoyt@github.com>
2013-12-21 21:21:39 -0800

    Remove user admin.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Foto::Application.routes.draw do
   root :to => "home#index"
-  resources :users, :only => [:index, :show, :edit, :update ]
   get '/auth/:provider/callback' => 'sessions#create'
   get '/signin' => 'sessions#new', :as => :signin
   get '/signout' => 'sessions#destroy', :as => :signout
