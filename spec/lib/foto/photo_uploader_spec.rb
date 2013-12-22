09fec72db4f7d5be9e42701e61d95162164ce0c9
Jonathan Hoyt <hoyt@github.com>
2013-12-22 15:05:31 -0800

    Add spec for uploading a tiff.

--- spec/lib/foto/photo_uploader_spec.rb
+++ spec/lib/foto/photo_uploader_spec.rb
@@ -25,4 +25,13 @@ describe Foto::PhotoUploader do
         eq("https://foto-test.s3.amazonaws.com/2012/4/5/2796cc91eeee95410b600ae7c12952bc.png")
     end
   end
+
+  describe "upload png" do
+    let(:photo) { path_to_fixture "johndbritton.tiff" }
+
+    it "uploads image using date, md5sum, and extension for path" do
+      expect(uploader.upload.public_url).to \
+        eq("https://foto-test.s3.amazonaws.com/2013/8/24/b2816cd510693da11786a9a8827d9254.tiff")
+    end
+  end
 end
