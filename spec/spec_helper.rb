f91dbb7b5c229c1fb002ca4d2b77d6bfe86ed417
Jonathan Hoyt <hoyt@github.com>
2013-12-22 10:25:52 -0800

    Set Fog to mock in specs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -38,9 +38,10 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
-  
+
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
+    Fog.mock!
   end
   config.before(:each) do
     DatabaseCleaner.start
