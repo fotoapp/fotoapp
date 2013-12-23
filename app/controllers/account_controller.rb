71aa1830857c1d24b8c94d2c0cc1dc99443858a8
Jonathan Hoyt <hoyt@github.com>
2013-12-22 17:23:34 -0800

    Fix method usage to comply with new API.
    This cleans up the deprecation warning in the tests right now.

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -3,6 +3,6 @@ class AccountController < ApplicationController
   before_filter :authenticate_user!
 
   def show
-    @photo_store = PhotoStore.find_or_initialize_by_user_id(current_user.id)
+    @photo_store = PhotoStore.find_or_initialize_by(:user_id => current_user.id)
   end
 end
