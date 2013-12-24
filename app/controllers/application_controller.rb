cd8134b8ca00e43a4f097aa01087ae1076356668
Jonathan Hoyt <hoyt@github.com>
2013-12-23 18:55:05 -0800

    Remove unused method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user
   helper_method :user_signed_in?
-  helper_method :correct_user?
 
   private
 
@@ -17,13 +16,6 @@ class ApplicationController < ActionController::Base
 
   def user_signed_in?
     return true if current_user
-  end
-
-  def correct_user?
-    @user = User.find(params[:id])
-    unless current_user == @user
-      redirect_to root_url, :alert => "Access denied."
-    end
   end
 
   def authenticate_user!
