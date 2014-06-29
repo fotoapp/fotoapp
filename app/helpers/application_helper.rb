d94685dbed30dbd72020783458c15589e690ccf3
Jonathan Hoyt <hoyt@github.com>
2014-06-29 08:16:45 -0700

    Show photos in grid with a little style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def clear_fixer(index)
+    return if index == 0
+
+    index = index + 1
+    classes = ["clearfix"]
+    if index % 4 == 0
+      classes << "visible-lg"
+    elsif index % 3 == 0
+      classes << "visible-md"
+    elsif index % 2 == 0
+      classes << "visible-sm"
+    end
+
+    return if classes.length == 1
+
+    content_tag :div, "", :class => classes.join(" ")
+  end
 end
