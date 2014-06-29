c6e8ba35b029307ebe7c7dd71d85c780bf3581e5
Jonathan Hoyt <hoyt@github.com>
2014-06-28 18:45:19 -0700

    Move photo_spec to photo_test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,7 +9,7 @@ class ActiveSupport::TestCase
   #
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
   # -- they do not yet inherit this setting
-  fixtures :all
+  fixtures :photo_stores, :photos, :users
 
   def setup
     Fog.mock!
